091702cf1e2337fddecceb58fbeaf059ac2edcec
Arbaz Mohammed <arbazkm.95@gmail.com>
2024-02-19 18:44:14 -0600

    Assignment-1

---
+++ Assignment-1/hello.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Enter your first name"
+read F_name
+echo "Enter your last name"
+read L_name
+
+echo "Hello $F_name $L_name to the system."
+ 
---
+++ Assignment-1/pp.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "This is the script name: $0"
+echo "This is 1st argument: $1"
+echo "This is 2nd argument: $2"
