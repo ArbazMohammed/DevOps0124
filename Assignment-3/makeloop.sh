26c68080d57f5e4cc2faa7541e014d76a1c707be
ArbazMohammed <158602313+ArbazMohammed@users.noreply.github.com>
2024-02-20 12:11:09 -0600

    Update makeloop.sh

--- Assignment-3/makeloop.sh
+++ Assignment-3/makeloop.sh
@@ -3,11 +3,11 @@
 echo "Enter any number"
 read num
 
-if [[ $num > 100 ]] && [[ $num < 200 ]]
+if [[ $num -gt 100 ]] && [[ $num -lt 200 ]]
 then
 
 echo "The number lies between 100 and 200"
-elif [[ $num < 100 ]]; then
+elif [[ $num -lt 100 ]]; then
 echo "The number is less than 100"
 else echo "The number is above 200"
 fi
